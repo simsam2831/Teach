e2eb84d87849ef762409a110e447d44033198631
simsam2831 <simondaniel2831@gmail.com>
2024-04-07 16:21:15 +0200

    TOKEN + FIRST BACKEND DONE

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Professor, School
 from .serializers import ProfessorSerializer, SchoolSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 
 class ProfessorViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Professor.objects.all()
     serializer_class = ProfessorSerializer
 
--- teach_project/settings.py
+++ teach_project/settings.py
@@ -31,15 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
+    'rest_framework',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
